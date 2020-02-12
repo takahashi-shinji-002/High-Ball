b77fd7eff786d90bdb8ded03556c72ef603a5729
takahashi-shinhi-002 <switchscratch015@gmail.com>
2020-02-12 16:16:59 +0900

    1回目のコミット

---
+++ Assets/Script/BallScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    private float Speed = 250;
+
+	// Use this for initialization
+	void Start ()
+    {
+		//  RigidBodyの取得
+        Rigidbody rb = GetComponent<Rigidbody>();
+        //  力を設定
+        Vector3 force = new Vector3(10.0f, 10,0f)*Speed;
+        //  力を加える
+        rb.AddForce(force);
+	}
+}
---
+++ Assets/Script/BarScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Update ()
+    {
+        //  Barの座標を取る
+        Transform BarTrans = GameObject.Find("Bar").transform;
+        Vector3 pos = BarTrans.position;
+
+        //  左キーを入力すると左にBarが動く
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(pos.x > -15)
+            {
+                this.transform.Translate(-0.8f, 0f, 0f);
+            }
+            
+        }
+        //  右キーを入力すると右にBarが動く
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(pos.x < 15)
+            {
+                this.transform.Translate(0.8f, 0f, 0f);
+            }
+            
+        }
+	}
+}
